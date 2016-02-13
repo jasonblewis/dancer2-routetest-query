0636de2137344ab9cfc912a8671e337cc050446e
Jason Lewis <jason@dickson.st>
2016-02-13 22:22:00 +1100

    add some documentation

--- lib/routetest.pm
+++ lib/routetest.pm
@@ -3,20 +3,24 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
+#show user a menu
 get '/' => sub {
     template 'index';
 };
 
+# don't know which ID user wants yet, so ask them for the ID, then redirect to the real report1
 get '/report1' => sub {
   template 'getid',
     {target => '/report1'};
 };
 
+# don't know which ID user wants yet, so ask them for the ID, then redirect to the real report2
 get '/report2' => sub {
   template 'getid',
     {target => '/report2'};
 };
 
+#use a post to hand the id to report1
 post '/report1' => sub {
   my $id = param('id');
   template 'report1', { id => $id };
